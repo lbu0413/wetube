b37079559be50bb715331827379eb647f29c6f0f
LBU <vvookly@gmail.com>
2021-05-26 14:22:10 -0400

    adding Middlewares

--- src/server.js
+++ src/server.js
@@ -3,15 +3,20 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-const handleHome = (req, res) => {
-	return res.send("<h1>hello world</h1>");
+const gossipMiddleware = (req, res, next) => {
+	console.log(`Someone is going to: ${req.url}`);
+	next();
+};
+
+const handleHome = (req, res, next) => {
+	return res.send("I love middlewares");
 };
 
 const handleLogin = (req, res) => {
 	return res.send("login here");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`listening on port ${PORT}`);
