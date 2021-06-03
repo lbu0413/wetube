6889bfafd13ca2235bc3789e306ef6cb7b99dd4f
LBU <vvookly@gmail.com>
2021-06-03 15:04:44 -0400

    added edit functionality using fake DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,50 @@
-const fakeUser = {
-	username: "Wook",
-	loggedIn: false,
-};
+let videos = [
+	{
+		title: "First Video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 59,
+		id: 1,
+	},
+	{
+		title: "Second Video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 59,
+		id: 2,
+	},
+	{
+		title: "Third Video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 59,
+		id: 3,
+	},
+];
 
 export const trending = (req, res) => {
-	const videos = [
-		{
-			title: "First Video",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 mins ago",
-			views: 59,
-			id: 1,
-		},
-		{
-			title: "Second Video",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 mins ago",
-			views: 59,
-			id: 1,
-		},
-		{
-			title: "Third Video",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 mins ago",
-			views: 59,
-			id: 1,
-		},
-	];
 	return res.render("home", { pageTitle: "Home", videos: videos });
 };
-export const view = (req, res) => res.render("view");
-export const edit = (req, res) => res.render("edit");
+export const view = (req, res) => {
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render("view", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+	const { id } = req.params;
+	const { title } = req.body;
+	videos[id - 1].title = title;
+	return res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
