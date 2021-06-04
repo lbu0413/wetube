5c9a090626be859e916a236edf1015561723f665
LBU <vvookly@gmail.com>
2021-06-04 16:42:36 -0400

    installed mongoose into project to use mongoDB

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("Connected to DB ✅");
+const handleError = (err) => console.log("DB Error", err);
+db.on("error", handleError);
+db.once("open", handleOpen);
