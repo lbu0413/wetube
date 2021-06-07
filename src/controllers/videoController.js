90e2d54966ed4e1cff27c6eed9820e0f276aac9c
LBU <vvookly@gmail.com>
2021-06-07 19:27:59 -0400

    db created for real and added a real db instead of fake db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,22 +2,20 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
 	const videos = await Video.find({});
-	console.log(videos);
-	return res.render("home", { pageTitle: "Home", videos: [] });
+	return res.render("home", { pageTitle: "Home", videos });
 };
 export const view = (req, res) => {
 	const { id } = req.params;
-	return res.render("view", { pageTitle: `Watching ${video.title}`, video });
+	return res.render("view", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
 	const { id } = req.params;
-	return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+	return res.render("edit", { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
 	const { id } = req.params;
 	const { title } = req.body;
-	videos[id - 1].title = title;
 	return res.redirect(`/videos/${id}`);
 };
 
@@ -25,9 +23,21 @@ export const getUpload = (req, res) => {
 	return res.render("upload", { pageTitle: "Upload video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
 	// here we will add a video to the videos array
-	const { title } = req.body;
+	const { title, description, hashtags } = req.body;
+	await Video.create({
+		title,
+		description,
+		createdAt: Date.now(),
+		hashtags: hashtags.split(",").map((word) => `#${word}`),
+		meta: {
+			views: 0,
+			rating: 0,
+		},
+	});
+
+	return res.redirect("/");
 };
 export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
